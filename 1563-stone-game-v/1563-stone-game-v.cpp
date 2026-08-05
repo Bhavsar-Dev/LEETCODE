dd23a430691173050b87e328d49ce17ffa447616
Dev Bhavsar <bhavsardev61@gmail.com>
2026-08-05 16:47:45 +0530

    Time: 515 ms (46.98%), Space: 27.6 MB (51.17%) - LeetHub

---
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+vector<int> pre;
+vector<vector<int>> dp;
+int sum(int l, int r) {
+    return pre[r + 1] - pre[l];
+}
+int solve(int l, int r) {
+    if(l == r) return 0;
+    if(dp[l][r] != -1) return dp[l][r];
+    int ans = 0;
+    for(int k = l; k < r; ++k) {
+        int left = sum(l,k);
+        int right = sum(k + 1, r);
+        if(left < right) ans = max(ans, left + solve(l, k));
+        else if(left > right) ans = max(ans, right + solve(k + 1, r));
+        else {
+            ans = max(ans, max(left + solve(l, k), right + solve(k + 1, r)));
+        }
+    }
+    return dp[l][r] = ans;
+}
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        pre.assign(n + 1, 0);
+        for(int i = 0; i < n; ++i) {
+            pre[i + 1] = pre[i] + stoneValue[i];
+        }
+        dp.assign(n, vector<int>(n, -1));
+        return solve(0, n - 1);
+    }
+};
