5aaeb8be5222fda377786abe2913fd0fa541e55b
Dev Bhavsar <bhavsardev61@gmail.com>
2026-08-15 21:32:51 +0530

    Time: 32 ms (65.24%), Space: 44.6 MB (41.36%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -12,19 +12,33 @@
 //     }
 // }
 
+// class Solution {
+//     int f(int n, int[] dp) {
+//         if(n == 0) return 0;
+//         if(dp[n] != -1) return dp[n];
+//         int ans = Integer.MAX_VALUE;
+//         for(int i = 1; i * i <= n; ++i) {
+//             ans = Math.min(ans, 1 + f(n - i * i, dp));
+//         }
+//         return dp[n] = ans;
+//     }
+//     public int numSquares(int n) {
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+//         return f(n, dp);
+//     }
+// }
+
 class Solution {
-    int f(int n, int[] dp) {
-        if(n == 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        int ans = Integer.MAX_VALUE;
-        for(int i = 1; i * i <= n; ++i) {
-            ans = Math.min(ans, 1 + f(n - i * i, dp));
-        }
-        return dp[n] = ans;
-    }
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return f(n, dp);
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= n; ++i) {
+            for(int j = 1; j * j <= i; ++j) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            } 
+        }
+        return dp[n];
     }
 }
